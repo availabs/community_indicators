58c75f0ae2ab9cc5f5c6a1e58c501ad394f048a6
nayanikabhargava <nayanikabhargava1993@gmail.com>
2019-02-14 15:22:51 -0500

    working for year, geolevel, measure and submeasure

--- src/pages/MapPage/layers/acsLayer.js
+++ src/pages/MapPage/layers/acsLayer.js
@@ -23,104 +23,206 @@ const NON_ORDINAL_LEGEND = {
     vertical: false,
     range: QUANTILE_RANGE
 }
-const counties = ['36001','36083','36093','36091','36039','360021','36115','36113'];
-
+const counties = ['36001','36083','36093','36091','36039','36021','36115','36113'];
 const year = [2014,2015,2016,2017,2018];
+let countyCousubs =[]
+let countyTracts =[]
+let acsCensusCousubs =[]
+let acsCensusTracts =[]
 class acsLayer extends MapLayer{
 
-    /* commented it to debug the below functions to understand Response code :0git
+
     onAdd(map) {
-        console.log('in add hello')
         super.onAdd(map)
         this.loading = true;
-        console.log('loading...')
-        return falcorGraph.get(['acs','config'])
+        return falcorGraph.get(['acs', 'config'])
             .then(res => {
-            console.log(typeof res)
-            //let censusConfig = ;
-            console.log('censusConfig')
-        })
+                let censusConfig = res.json.acs.config
+                this.filters.measures.domain = Object.keys(censusConfig).map(function(key){
+                    return {name: censusConfig[key].name,value:key}
+
+                })
+                this.filters.submeasure.domain = censusConfig[this.filters.measures.value]
+                    .variables.map(function(subvar){
+                        return {name: subvar.name,value:subvar.value}
+                    })
 
 
-        /*
-        .then(res => {
-            var censusConfig = res.json.acs.config.key;
-            console.log('censusConfig', censusConfig)
-        })
-         */
-
-    fetchData(){
-        console.log('got to fetch data');
-        return falcorGraph.get(
-            ['geo', counties, [this.filters.year.value], [this.filters.measures.value]]
-        ).then(data => {
-            console.log('fetch data',data);
-            return data
-        })
+                this.acsConfig = censusConfig
+                this.filters.geolevel.domain = ['counties','cousubs','tracts']
+                //console.log('I get here')
+                return this.fetchData()
+                    .then(data => this.receiveData(map, data))
+                    .then(() => this.loading = false)
+            })
 
     }
+
+    fetchData(){
+        console.log('fetch data')
+        return falcorGraph.get(
+            ['acs',counties,[this.filters.year.value],[this.filters.measures.value]],
+            ['geo',counties,[this.filters.geolevel.value]]
+        ).then(data => {
+            console.log('first data', data)
+            if (this.filters.geolevel.value === 'counties') {
+                console.log('gonna return data')
+                return data
+            }
+
+        else if (this.filters.geolevel.value === 'cousubs') {
+            Object.values(data.json.geo).forEach(function (acsGeoLevel) {
+                countyCousubs.push(acsGeoLevel.cousubs)
+
+            })
+            acsCensusCousubs = countyCousubs.flat(1)
+        }
+
+        else{
+            Object.values(data.json.geo).forEach(function(acsGeoLevel){
+                countyTracts.push(acsGeoLevel.tracts)
+
+            })
+            acsCensusTracts = countyTracts.flat(1)
+        }
+
+        if(this.filters.geolevel.value === 'cousubs'){
+            return falcorGraph.get(
+                ['acs', acsCensusCousubs, [this.filters.year.value], [this.filters.measures.value]]
+            ).then(res => {
+            return res
+        })
+        }
+
+        if (this.filters.geolevel.value === 'tracts') {
+            return falcorGraph.get(
+                ['acs', acsCensusTracts, [this.filters.year.value], [this.filters.measures.value]]
+            ).then(res => {
+                return res
+            })
+        }
+        })
+        }
+
+
     receiveData(map,data){
-        console.log('receive data',data);
+        console.log('recieve data called', data)
+        const geolevel = this.filters.geolevel.value;
+        let graph = data.json.acs,
 
+        values = {},
+        colors = {},
 
-               let graph = data.json.geo,
+        domain = [];
+        let min = Infinity,
+        max = -Infinity,
 
-                values = {},
-                colors = {},
-
-                domain = [];
-
-            let min = Infinity,
-                max = -Infinity,
-
-                scale;
-
+        scale;
+        if(geolevel === 'counties'){
             counties.forEach(geoid => {
-                const value = graph[geoid][this.filters.year.value][this.filters.measures.value];
-            values[geoid] = value;
-            domain.push(value);
+                let acsVar = graph[geoid][this.filters.year.value][this.filters.measures.value]
+                let subVar = [this.filters.submeasure.value]
+                let value = acsVar[subVar]
+                domain.push(value);
+            values[geoid] = value
+
             min = Math.min(min, value);
+
             max = Math.max(max, value);
         })
+            map.setFilter('counties',['in','geoid',...counties])
+            map.setFilter('cousubs',['in','geoid', ''])
+            map.setFilter('tracts',['in','geoid', ''])
+        }
 
-            const type = this.legend.type,
-                range = this.legend.range;
-            switch (type) {
-                case "quantile":
-                    scale = scaleQuantile()
-                        .domain(domain)
-                        .range(range);
-                    this.legend.domain = domain;
-                    break;
-                case "quantize":
-                    scale = scaleQuantize()
-                        .domain([min, max])
-                        .range(range);
-                    this.legend.domain = [min, max];
-                    break;
-            }
+        if(geolevel === 'cousubs'){
+            acsCensusCousubs.forEach(cousub => {
+                if (cousub !== undefined){
+                let acsVar = graph[cousub][this.filters.year.value][this.filters.measures.value]
+                let subVar = [this.filters.submeasure.value]
+                let value = acsVar[subVar]
+                domain.push(value);
+                values[cousub] = value
 
-            for (const pid in values) {
-                colors[pid] = scale(values[pid])
+                min = Math.min(min, value);
+
+                max = Math.max(max, value);
             }
-            console.log(colors);
-            map.setPaintProperty('counties','fill-color',['get',['to-string',['get','geoid']],['literal',colors]])
+        })
+            map.setFilter('cousubs',['in','geoid',...acsCensusCousubs.filter(d => d)])
+            map.setFilter('counties',['in','geoid', ''])
+            map.setFilter('tracts',['in','geoid', ''])
+        }
+        if (geolevel === 'tracts'){
+            acsCensusTracts.forEach(tract => {
+                if (tract !== undefined){
+                let acsVar = graph[tract][this.filters.year.value][this.filters.measures.value]
+                let subVar = [this.filters.submeasure.value]
+                let value = acsVar[subVar]
+                domain.push(value);
+                values[tract] = value
+
+                min = Math.min(min, value);
+
+                max = Math.max(max, value);
+            }
+        })
+            map.setFilter('tracts',['in','geoid',...acsCensusTracts.filter(d => d)])
+            map.setFilter('cousubs',['in','geoid', ''])
+            map.setFilter('counties',['in','geoid', ''])
+
+        }
+        const type = this.legend.type,
+            range = this.legend.range;
+        switch (type) {
+            case "quantile":
+                scale = scaleQuantile()
+                    .domain(domain)
+                    .range(range);
+                this.legend.domain = domain;
+                break;
+            case "quantize":
+                scale = scaleQuantize()
+                    .domain([min, max])
+                    .range(range);
+                this.legend.domain = [min, max];
+                break;
+        }
+        for (const pid in values) {
+            colors[pid] = scale(values[pid])
+        }
+
+            map.setPaintProperty(this.filters.geolevel.value,'fill-color',['get',['to-string',['get','geoid']],['literal',colors]])
+
         }
 
 }
 
-
-
 const buildingsLayer = new acsLayer("ACS Layer", {
         active: true,
+        loading: true,
         sources: [
             { id: "counties",
                 source: {
                     'type': "vector",
                     'url': 'mapbox://am3081.1ggw4eku'
-                }
+                },
+            },
+            { id: "cousubs",
+                source: {
+                    'type': "vector",
+                    'url': 'mapbox://am3081.dlnvkxdi'
+                },
+            },
+            { id: "tracts",
+                source: {
+                    'type': "vector",
+                    'url': 'mapbox://am3081.92hcxki8'
+                },
             }
+
         ],
+
         layers: [
             { 'id': 'counties',
                 'source': 'counties',
@@ -130,6 +232,24 @@ const buildingsLayer = new acsLayer("ACS Layer", {
                     'fill-color': 'rgba(196, 0, 0, 0.1)',
                 },
                 filter : ['all',['in','geoid',...counties]]
+            },
+            { 'id': 'cousubs',
+                'source': 'cousubs',
+                'source-layer': 'cousubs',
+                'type': 'fill',
+                'paint': {
+                    'fill-color': 'rgba(195, 0, 0, 0.1)',
+                },
+                filter : ['in','geoid','']
+            },
+            { 'id': 'tracts',
+                'source': 'tracts',
+                'source-layer': 'tracts',
+                'type': 'fill',
+                'paint': {
+                    'fill-color': 'rgba(196, 0, 0, 0.1)',
+                },
+                filter : ['in','geoid','']
             }
         ],
 
@@ -155,15 +275,45 @@ filters: {
             year: {
                 name: 'year',
                 type: 'dropdown',
-                domain: [2015,2016,2017],
-                value: 2017
+                domain: [2014,2015,2016,2017],
+                value: 2014
+            },
+            geolevel:{
+                name: 'geolevel',
+                    type: 'dropdown',
+                    domain: [],
+                    value: 'counties'
             },
             measures: {
                 name: "measures",
                 type: "dropdown",
-                domain: ['population','poverty','non_english_speaking','under_5','over_64','vulnerable','population_change','poverty_change','non_english_speaking_change','under_5_change','over_64_change','vulnerable_change'],
-                value: "population"
-    }
+                domain: [],
+                value: "B01003",
+                    onChange: (map, layer, value) => {
+                    Object.keys(layer.acsConfig).forEach(function(item){
+                        if (value === item){
+
+
+                            layer.filters.submeasure = {
+                                    name:'submeasure',
+                                    type: 'dropdown',
+                                    domain: layer.acsConfig[item].variables.map(d => {
+                                        return {name:d.name, value:d.value}
+                                    }),
+                                    value: layer.acsConfig[item].variables.map(d => d.value)[0]
+                                }
+                        }
+                    })
+                    
+                    layer.component.forceUpdate()
+                }
+            },
+            submeasure: {
+                name:'submeasure',
+                    type: 'dropdown',
+                    domain: [],
+                    value: 'B01003_001E'
+            }
 
 },
 
