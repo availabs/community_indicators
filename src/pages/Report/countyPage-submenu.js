54586f072b5d3da4ff80de908541fec45c5cc80f
nayanika2304 <nayanikabhargava1993@gmail.com>
2019-05-07 14:46:49 -0400

    submenus for countyPage

---
+++ src/pages/Report/countyPage-submenu.js
@@ -0,0 +1,26 @@
+export default
+[
+    [
+        {name: 'Albany County', path: '/report/36001',
+            children: [
+                {name: 'Albany City',  path: '/report/3600101000'},
+                {name: 'Berne town', path: '/report/3600106211'},
+                {name: 'Bethlehem town', path: '/report/3600106354'},
+                {name: 'Coeymans town', path: '/report/3600116694'},
+                {name: 'Cohoes City',path: '/report/3600116749'},
+                {name: 'Colonie town',path: '/report/3600117343'}
+            ]
+        },
+        {name: 'Rensselaer County', path: '/report/36083',
+            children: [
+                {name:'Berlin town', path: '/report/3608306189'}
+            ]
+        },
+        {name: 'Schenectady County', path: '/report/36093'},
+        {name: 'Saratoga County', path: '/report/36091'},
+        {name: 'Greene County', path: '/report/36039'},
+        {name: 'Columbia County', path: '/report/36021'},
+        {name: 'Washington County', path: '/report/36115'},
+        {name: 'Warren County', path: '/report/36113'},
+    ]
+]
