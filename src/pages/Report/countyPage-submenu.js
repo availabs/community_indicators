5e60a95cf7ee05a13817c5af2f5c3aaa0fe7f8c1
nayanika2304 <nayanikabhargava1993@gmail.com>
2019-05-08 11:06:03 -0400

    added cities

--- src/pages/Report/countyPage-submenu.js
+++ src/pages/Report/countyPage-submenu.js
@@ -5,10 +5,7 @@ export default
             children: [
                 {name: 'Albany City',  path: '/report/3600101000'},
                 {name: 'Berne town', path: '/report/3600106211'},
-                {name: 'Bethlehem town', path: '/report/3600106354'},
-                {name: 'Coeymans town', path: '/report/3600116694'},
-                {name: 'Cohoes City',path: '/report/3600116749'},
-                {name: 'Colonie town',path: '/report/3600117343'}
+                {name: 'Bethlehem town', path: '/report/3600106354'}
             ]
         },
         {name: 'Rensselaer County', path: '/report/36083',
@@ -16,11 +13,42 @@ export default
                 {name:'Berlin town', path: '/report/3608306189'}
             ]
         },
-        {name: 'Schenectady County', path: '/report/36093'},
-        {name: 'Saratoga County', path: '/report/36091'},
-        {name: 'Greene County', path: '/report/36039'},
-        {name: 'Columbia County', path: '/report/36021'},
-        {name: 'Washington County', path: '/report/36115'},
-        {name: 'Warren County', path: '/report/36113'},
+        {name: 'Schenectady County', path: '/report/36093',
+
+            children: [
+                {name: 'Albany City',  path: '/report/3600101000'},
+                {name: 'Berne town', path: '/report/3600106211'}
+            ]},
+        {name: 'Saratoga County', path: '/report/36091',
+
+            children: [
+                {name: 'Cohoes City',path: '/report/3600116749'}
+            ]
+        },
+        {name: 'Greene County', path: '/report/36039',
+
+            children: [
+                {name: 'Bethlehem town', path: '/report/3600106354'}
+            ]
+        },
+        {name: 'Columbia County', path: '/report/36021',
+
+            children: [
+                {name: 'Bethlehem town', path: '/report/3600106354'},
+            ]
+        },
+        {name: 'Washington County', path: '/report/36115',
+
+            children: [
+
+                {name: 'Coeymans town', path: '/report/3600116694'}
+            ]
+        },
+        {name: 'Warren County', path: '/report/36113',
+
+            children: [
+                {name: 'Colonie town',path: '/report/3600117343'}
+            ]
+        },
     ]
 ]
