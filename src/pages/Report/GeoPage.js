bbafea61c8892f062365669f1aebd7989a326baa
nayanika2304 <nayanikabhargava1993@gmail.com>
2019-05-20 10:10:09 -0400

    updates

--- src/pages/Report/GeoPage.js
+++ src/pages/Report/GeoPage.js
@@ -6,48 +6,92 @@ import CensusStackedLineChart from 'components/censusCharts/line/censusStackedLi
 import CensusMultiStackedLineChart from 'components/censusCharts/line/censusMultiStackedLineChart'
 import CensusPieChart from 'components/censusCharts/pie/censusPieChart'
 import CensusStatBox from 'components/censusCharts/statBox/censusStatBox.js'
+import GridLayout from 'pages/Analysis/GraphLayout/GridLayout.js'
+import subMenus from 'pages/Report/GeoPage-submenu.js'
 
-import subMenus from 'pages/Report/countyPage-submenu.js'
+const GRAPH_CONFIG = require('pages/Report/graphConfig.js')
 
 class GeoPage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = ({
+            graphConfig : []
+        })
+    }
+
+    componentWillMount(){
+        this.setState({
+
+        })
+    }
     render(){
         let geoid = this.props.match.params.geoid ?
             this.props.match.params.geoid
             :36001;
+        const style ={
+            height: 1000
+        }
         return (
             <div>
-            <h4>Demographics</h4>
             <div class="Demographics" style={{color:'rgba(0,0,0,.8)',fontSize:'12px',fontWeight: '400',display: 'flex', alignContent: 'center', alignItems:'center', justifyContent:'flex-start',padding:'5px'}}>
-            <CensusStatBox geoid={[geoid]} censusKey={['B01003']} year={['2017']}/>
-            <CensusStatBox geoid={[geoid]} censusKey={['B01002']} year={['2017']}/>
-            </div>
-            <h1> Racial Population for the year: 2017 </h1>
-            <CensusPieChart geoid={[geoid]} censusKey={['B02001']} year={['2017']}/>
-            <div class="Economy" style={{color:'rgba(0,0,0,.8)',fontSize:'12px',fontWeight: '400',display: 'flex', alignContent: 'center', alignItems:'center', justifyContent:'flex-start',flexWrap: 'wrap',flexDirection:'row',margin: '0 5px'}}>
-            <CensusLineChart geoid={[geoid]} censusKey={['B19013']}/>
-            <h4>Economy
-            <CensusStatBox geoid={[geoid]} censusKey={['B19013']} amount={true}/>
-            </h4>
-            </div>
-            <div class="Poverty" style={{color:'rgba(0,0,0,.8)',fontSize:'12px',fontWeight: '400',display: 'flex', alignContent: 'center', alignItems:'center', justifyContent:'flex-start',flexWrap: 'wrap',flexDirection:'row',margin: '0 5px'}}>
-            <CensusLineChart geoid={[geoid]} censusKey={['B17001']}/>
-            <h4>Poverty
-            <CensusStatBox geoid={[geoid]} censusKey={['B17001']} poverty={true}/>
-            </h4>
-            </div>
-            <div class="Housing" style={{color:'rgba(0,0,0,.8)',fontSize:'12px',fontWeight: '400',display: 'flex', alignContent: 'center', alignItems:'center', justifyContent:'flex-start',flexWrap: 'wrap',flexDirection:'row',margin: '0 5px'}}>
-            <CensusStackedLineChart geoid={[geoid]} censusKey={['B25002']}/>
-            <h4>Housing
-            <CensusStatBox geoid={[geoid]} censusKey={['B25002']} housing={true}/>
-            </h4>
-            </div>
-            <div class="Education" style={{color:'rgba(0,0,0,.8)',fontSize:'12px',fontWeight: '400',display: 'flex', alignContent: 'center', alignItems:'center', justifyContent:'flex-start',flexWrap: 'wrap',flexDirection:'row',margin: '0 5px'}}>
-            <CensusMultiStackedLineChart geoid={[geoid]} censusKey={['B15003']}/>
-            <h4>Education
-            <CensusStatBox geoid={[geoid]} censusKey={['B15003']} education={true}/>
-            </h4>
-            </div>
-            </div>
+    <CensusStatBox geoid={[geoid]} censusKey={['B01003']} year={['2017']} demographics={true}/>
+        <CensusStatBox geoid={[geoid]} censusKey={['B01002']} year={['2017']} demographics={true}/>
+        </div>
+        <h1> Racial Population for the year: 2017 </h1>
+        <CensusPieChart geoid={[geoid]} censusKey={['B02001']} year={['2017']} single={true}/>
+        <div class="Economy" style={{color:'rgba(0,0,0,.8)',fontSize:'12px',fontWeight: '400',display: 'flex', alignContent: 'center', alignItems:'center', justifyContent:'flex-start',flexWrap: 'wrap',flexDirection:'row',margin: '0 5px'}}>
+    <GridLayout
+        sideWidth={300}
+        minifiedWidth={1}
+        isOpen={1}
+        title={''}
+        viewing={false}
+        graphs={[{
+                id :'1',
+                type:'CensusLineChart',
+                geoid:['36001'],
+                censusKey:['B19013'],
+                layout:{
+                    static:false,
+                    h: 14,
+                    w: 6,
+                    x:0,
+                    y:0
+                }
+            },
+        {
+            id :'2',
+                type:'CensusStatBox',
+            geoid:['36001'],
+            censusKey:['B19013'],
+            amount:true,
+            layout:{
+            static:false,
+                h: 5,
+                w: 2,
+                x:0,
+                y:0
+        }
+        }
+
+    ]}
+        onOpenOrClose={function noop() {}}
+        onLayoutChange={ this.props.onLayoutChange }
+        />
+        </div>
+        <div class="Poverty" style={{color:'rgba(0,0,0,.8)',fontSize:'12px',fontWeight: '400',display: 'flex', alignContent: 'center', alignItems:'center', justifyContent:'flex-start',flexWrap: 'wrap',flexDirection:'row',margin: '0 5px'}}>
+    <CensusLineChart geoid={[geoid]} censusKey={['B17001']}/>
+        <CensusStatBox geoid={[geoid]} censusKey={['B17001']} poverty={true}/>
+        </div>
+        <div class="Housing" style={{color:'rgba(0,0,0,.8)',fontSize:'12px',fontWeight: '400',display: 'flex', alignContent: 'center', alignItems:'center', justifyContent:'flex-start',flexWrap: 'wrap',flexDirection:'row',margin: '0 5px'}}>
+    <CensusStackedLineChart geoid={[geoid]} censusKey={['B25002']}/>
+        <CensusStatBox geoid={[geoid]} censusKey={['B25002']} housing={true}/>
+        </div>
+        <div class="Education" style={{color:'rgba(0,0,0,.8)',fontSize:'12px',fontWeight: '400',display: 'flex', alignContent: 'center', alignItems:'center', justifyContent:'flex-start',flexWrap: 'wrap',flexDirection:'row',margin: '0 5px'}}>
+    <CensusMultiStackedLineChart geoid={[geoid]} censusKey={['B15003']}/>
+        <CensusStatBox geoid={[geoid]} censusKey={['B15003']} education={true}/>
+        </div>
+        </div>
         )
     }
 
@@ -65,7 +109,7 @@ const mapDispatchToProps = {};
 export default
     {
         path: '/report/:geoid',
-        name: 'County Page',
+        name: 'Geo Page',
         mainNav: false,
         breadcrumbs: [
             {param: 'geoid', path: '/report/'}
@@ -82,3 +126,7 @@ export default
         subMenus: subMenus,
         component: connect(mapStateToProps, mapDispatchToProps)(reduxFalcor(GeoPage))
     }
+
+/*
+
+ */
