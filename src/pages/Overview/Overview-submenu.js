d36daf739528f8cb60d5b5746227309b26ba2704
nayanika2304 <nayanikabhargava1993@gmail.com>
2019-05-23 14:53:00 -0400

    Overview-submenu

---
+++ src/pages/Overview/Overview-submenu.js
@@ -0,0 +1,54 @@
+export default
+[
+    [
+        {name: 'Albany County', path: '/overview/36001',
+            children: [
+                {name: 'Albany City',  path: '/overview/3600101000'},
+                {name: 'Berne town', path: '/overview/3600106211'},
+                {name: 'Bethlehem town', path: '/overview/3600106354'}
+            ]
+        },
+        {name: 'Rensselaer County', path: '/overview/36083',
+            children: [
+                {name:'Berlin town', path: '/overview/3608306189'}
+            ]
+        },
+        {name: 'Schenectady County', path: '/overview/36093',
+
+            children: [
+                {name: 'Albany City',  path: '/overview/3600101000'},
+                {name: 'Berne town', path: '/overview/3600106211'}
+            ]},
+        {name: 'Saratoga County', path: '/overview/36091',
+
+            children: [
+                {name: 'Cohoes City',path: '/overview/3600116749'}
+            ]
+        },
+        {name: 'Greene County', path: '/overview/36039',
+
+            children: [
+                {name: 'Bethlehem town', path: '/overview/3600106354'}
+            ]
+        },
+        {name: 'Columbia County', path: '/overview/36021',
+
+            children: [
+                {name: 'Bethlehem town', path: '/overview/3600106354'},
+            ]
+        },
+        {name: 'Washington County', path: '/overview/36115',
+
+            children: [
+
+                {name: 'Coeymans town', path: '/overview/3600116694'}
+            ]
+        },
+        {name: 'Warren County', path: '/overview/36113',
+
+            children: [
+                {name: 'Colonie town',path: '/overview/3600117343'}
+            ]
+        },
+    ]
+]
