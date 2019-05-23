c6c62739eadf77cf0607a4f6107e56bba8374661
nayanika2304 <nayanikabhargava1993@gmail.com>
2019-05-23 14:50:06 -0400

    social-welfare submenu

---
+++ src/pages/SocialWelfare/SocialWelfare-submenu.js
@@ -0,0 +1,54 @@
+export default
+[
+    [
+        {name: 'Albany County', path: '/social/36001',
+            children: [
+                {name: 'Albany City',  path: '/social/3600101000'},
+                {name: 'Berne town', path: '/social/3600106211'},
+                {name: 'Bethlehem town', path: '/social/3600106354'}
+            ]
+        },
+        {name: 'Rensselaer County', path: '/social/36083',
+            children: [
+                {name:'Berlin town', path: '/social/3608306189'}
+            ]
+        },
+        {name: 'Schenectady County', path: '/social/36093',
+
+            children: [
+                {name: 'Albany City',  path: '/social/3600101000'},
+                {name: 'Berne town', path: '/social/3600106211'}
+            ]},
+        {name: 'Saratoga County', path: '/social/36091',
+
+            children: [
+                {name: 'Cohoes City',path: '/social/3600116749'}
+            ]
+        },
+        {name: 'Greene County', path: '/social/36039',
+
+            children: [
+                {name: 'Bethlehem town', path: '/social/3600106354'}
+            ]
+        },
+        {name: 'Columbia County', path: '/social/36021',
+
+            children: [
+                {name: 'Bethlehem town', path: '/social/3600106354'},
+            ]
+        },
+        {name: 'Washington County', path: '/social/36115',
+
+            children: [
+
+                {name: 'Coeymans town', path: '/social/3600116694'}
+            ]
+        },
+        {name: 'Warren County', path: '/social/36113',
+
+            children: [
+                {name: 'Colonie town',path: '/social/3600117343'}
+            ]
+        },
+    ]
+]
