81d60d9a9b633c2142ec7b1ef29bc7aec873ab34
nayanika2304 <nayanikabhargava1993@gmail.com>
2019-05-28 15:00:34 -0400

    overview of single geography

--- src/pages/Report/Overview.js
+++ src/pages/Report/Overview.js
@@ -1,83 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { reduxFalcor } from 'utils/redux-falcor'
-import GridLayout from 'pages/Analysis/GraphLayout/GridLayout.js'
-import subMenus from 'pages/Overview/Overview-submenu.js'
-//import GraphLayout from 'pages/Analysis/GraphLayout'
-const GRAPH_CONFIG = require('pages/Report/graphConfig.js')
-
-class Overview extends React.Component{
-    constructor(props) {
+import Element from 'components/light-admin/containers/Element'
+import styled from "styled-components";
+import AvlMap from 'AvlMap'
+import TractsLayer from 'components/layers/TractsLayers.js'
+import CensusPieCompare from "components/censusCharts/pie/censusPieCompare"
+import CensusDomGraph from 'components/censusCharts/bar/censusDomGraph'
+class Home extends Component {
+    constructor(props){
         super(props);
-        this.state={
-            graphConfig:[]
+        this.state ={
+            update: {id:0}
         }
+        this.onMeasureClick = this.onMeasureClick.bind(this)
     }
-
-    componentWillMount(){
-        this.setState({
-            graphConfig: GRAPH_CONFIG
-        })
+    onMeasureClick (d) {
+        TractsLayer.filters.censvar.value = d;
+        let update = Object.assign({},this.state.update);
+        update.id = update.id+1;
+        this.setState({update})
     }
-
-    render(){
-        let geoid = this.props.match.params.geoid ?
-            this.props.match.params.geoid
-            :36001;
-        this.state.graphConfig.overview.map(function(config){
-            config['geoid'] = [geoid]
-        })
+    render () {
         return (
             <div>
-                <GridLayout
-                    sideWidth={800}
-                    minifiedWidth={1}
-                    isOpen={1}
-                    title={''}
-                    viewing={false}
-                    graphs={this.state.graphConfig.overview}
-                    onOpenOrClose={function noop() {}}
-                    onLayoutChange={ this.props.onLayoutChange }
-                    verticalCompact={false}
-            />
+            <Element>
+            <div style={{height:5000}}>
+    <h1>Racial Population</h1>
+        <CensusPieCompare geoid={['36001','36083','36093','36091','36039','36021','36115','36113']} censusKey={['B02001']}/>
+        <div style={{width: '90%', height: '100vh', display: 'flex', alignContent: 'stretch', alignItems:'stretch', marginLeft: 40}}>
+    <div style={{height: '100vh', width: '50vw'}}>
+    <div style={{padding: 20, position: 'relative'}}>
+    <CensusDomGraph
+        geoid={['36001','36083','36093']}
+        censusKey={['B02001']}
+        onClick={this.onMeasureClick} />
         </div>
+        </div>
+        <div style={{height: '90vh', width: '50%'}}>
+    <AvlMap
+        sidebar={false}
+        scrollZoom={false}
+        zoom={6}
+        update={[this.state.update]}
+        style={'mapbox://styles/am3081/cjt7mqq7b2r5f1fo0iw1zzgu7'}
+        fitBounds={[
+                [
+                    -75.84467427717281,
+                    38.538851525354666
+                ],
+            [
+                -70.7626953125,
+            45.042478050891546
+    ]]}
+        layers={[TractsLayer]}
+        />
+        </div>
+        </div>
+        </div>
+        </Element>
+        </div>
+
     )
     }
-
-    static defaultProps = {
-        width:[],
-        height:[]
-    }
-
 }
 
-const mapStateToProps = state => {
-    return {
-        geoGraph: state.graph.geo,
-        router: state.router
-    };
-};
-
-const mapDispatchToProps = {};
-
-export default
-{
-
-    path: '/overview/:geoid',
-    name: 'Overview Report',
-    mainNav: false,
-    breadcrumbs: [
-        {param: 'geoid', path: '/overview/'}
-    ],
-    menuSettings:
-        {
-            image: 'none',
-            scheme: 'color-scheme-dark',
-            position: 'menu-position-left',
-            layout: 'menu-layout-mini',
-            style: 'color-style-default',
-            subemenustyle: 'sub-menu-style-over'
-        },
-    subMenus: subMenus,
-    component: connect(mapStateToProps, mapDispatchToProps)(reduxFalcor(Overview))
+export default {
+    icon: 'os-icon-home',
+    path: '/overview',
+    exact: true,
+    mainNav: true,
+    menuSettings: {
+        image: 'none',
+        scheme: 'color-scheme-dark',
+        position: 'menu-position-left',
+        layout: 'menu-layout-mini',
+        style: 'color-style-default',
+        subemenustyle: 'sub-menu-style-over'
+    },
+    name: 'Overview',
+    auth: false,
+    component: Home
 }
+
