5aafc02e3584644086bf618f5b34b9df3e51b595
nayanika2304 <nayanikabhargava1993@gmail.com>
2019-05-28 14:59:59 -0400

    Report index with overview ad social welfare of the default county

--- src/pages/Report/index.js
+++ src/pages/Report/index.js
@@ -1,84 +1,97 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import Element from 'components/light-admin/containers/Element'
-import styled from "styled-components";
-import AvlMap from 'AvlMap'
-import TractsLayer from 'components/layers/TractsLayers.js'
-import CensusPieCompare from "components/censusCharts/pie/censusPieCompare"
-import CensusDomGraph from 'components/censusCharts/bar/censusDomGraph'
-class Home extends Component {
+import React, {Component} from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+//import GridLayout from './GridLayout'
+//import subMenus from './GeoReport-submenu.js'
+import GridLayout from '../Analysis/GraphLayout/GridLayout'
+import subMenus from './GeoPage-submenu.js'
+import '/home/nayanika/IdeaProjects/community_indicators/node_modules/react-grid-layout/css/styles.css'
+
+const GRAPH_CONFIG = require('pages/Report/graphConfig.js')
+
+const GraphLayoutContainer = styled.div`
+  height: 100vh;
+  overflow-y: scroll;
+  margin-left: ${ props => props.sideWidth }px;
+  transition: margin-left 250ms;
+  padding: 15px 30px;
+  
+`;
+
+class ReportIndex extends Component {
     constructor(props){
         super(props);
         this.state ={
-            update: {id:0}
+            graphConfig: []
         }
-        this.onMeasureClick = this.onMeasureClick.bind(this)
-    }
-    onMeasureClick (d) {
-        TractsLayer.filters.censvar.value = d;
-        let update = Object.assign({},this.state.update);
-        update.id = update.id+1;
-        this.setState({update})
-    }
-    render () {
-        return (
-            <div>
-            <Element>
-            <div style={{height:5000}}>
-            <h1>Racial Population</h1>
-            <CensusPieCompare geoid={['36001','36083','36093','36091','36039','36021','36115','36113']} censusKey={['B02001']}/>
-            <div style={{width: '90%', height: '100vh', display: 'flex', alignContent: 'stretch', alignItems:'stretch', marginLeft: 40}}>
-            <div style={{height: '100vh', width: '50vw'}}>
-            <div style={{padding: 20, position: 'relative'}}>
-                <CensusDomGraph
-                    geoid={['36001','36083','36093']}
-                    censusKey={['B02001']}
-                    onClick={this.onMeasureClick} />
-            </div>
-            </div>
-            <div style={{height: '90vh', width: '50%'}}>
-            <AvlMap
-                    sidebar={false}
-                    scrollZoom={false}
-                    zoom={6}
-                    update={[this.state.update]}
-                    style={'mapbox://styles/am3081/cjt7mqq7b2r5f1fo0iw1zzgu7'}
-                    fitBounds={[
-                            [
-                                -75.84467427717281,
-                                38.538851525354666
-                            ],
-                        [
-                            -70.7626953125,
-                        45.042478050891546
-                ]]}
-                    layers={[TractsLayer]}
-            />
-            </div>
-            </div>
-            </div>
-            </Element>
-            </div>
 
-    )
     }
+
+    static propTypes = {
+        width: PropTypes.number,
+        isOpen: PropTypes.number,
+        minifiedWidth: PropTypes.number,
+        onOpenOrClose: PropTypes.func
+    }
+
+    render() {
+        const { sideWidth, theme, viewing, title, isOpen, graph1,graph2, ...rest } = this.props;
+        this.props.graph1.map(function(graph){
+            graph['geoid'] = ['36001']
+        })
+
+        this.props.graph2.map(function(graph){
+            graph['geoid'] = ['36001']
+        })
+        return (
+        <GraphLayoutContainer
+        theme={ theme }
+        sideWidth={ viewing || !isOpen ? 0 : sideWidth }>
+
+        <div className='container'>
+            <h4>{ title }</h4>
+        </div>
+        <h1 align='center'> Overview </h1>
+        <GridLayout
+        graphs= {graph1}
+        viewing={ viewing }
+        {...rest}/>
+
+        <h1 align='center'> Social Welfare </h1>
+        <GridLayout
+        graphs= {graph2}
+        viewing={ viewing }
+        {...rest}/>
+        </GraphLayoutContainer>
+    );
+    }
+
+    static defaultProps = {
+        sideWidth: 300,
+        minifiedWidth: 1,
+        isOpen: 1,
+        title: '',
+        viewing: false,
+        graph1: GRAPH_CONFIG.overview,
+        graph2: GRAPH_CONFIG.socialWelfare,
+        onOpenOrClose: function noop() {}
+    };
 }
 
 export default {
     icon: 'os-icon-home',
     path: '/report',
-    exact: true,
-    mainNav: true,
+    mainNav:true,
+    exact:true,
     menuSettings: {
         image: 'none',
         scheme: 'color-scheme-dark',
         position: 'menu-position-left',
         layout: 'menu-layout-mini',
-        style: 'color-style-default',
-        subemenustyle: 'sub-menu-style-over'
+        style: 'color-style-default'
     },
     name: 'Report',
-    auth: false,
-    component: Home
+    auth:false,
+    subMenus: subMenus,
+    component: ReportIndex
 }
-
