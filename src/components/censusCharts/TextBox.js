e9ec4013b8cf3e7c5c89cb3c65089df67cdc493c
Eric Conklin <eric.s.conklin@gmail.com>
2019-09-19 13:00:58 -0400

    stuff

--- src/components/censusCharts/TextBox.js
+++ src/components/censusCharts/TextBox.js
@@ -2,30 +2,22 @@ import React from "react"
 
 import styled from "styled-components"
 
-const Footer = styled.div`
-position: relative;
-box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.2),
-            -1px -1px 2px 0 rgba(0, 0, 0, 0.1);
-padding: 10px 20px;
-margin-bottom: 10px;
-border-radius: 4px;
-`
 const Header = styled.div`
-font-size: 1.5rem;
+  font-size: 1.5rem;
 `
 const SubHeader = styled.div`
-font-size: 0.75rem;
-margin-top: -7px;
-margin-bottom: 5px;
+  font-size: 0.75rem;
+  margin-top: -7px;
+  margin-bottom: 5px;
 `
 const Body = styled.div`
-font-size: 1rem;
+  font-size: 1rem;
 `
 const Link = styled.a`
-font-size: 1rem;
-position: absolute;
-top: 10px;
-right: 20px;
+  font-size: 1rem;
+  position: absolute;
+  top: 10px;
+  right: 20px;
 `
 export default ({ header, subheader, body, link }) =>
   <div style={ { padding: "10px 20px" } }>
