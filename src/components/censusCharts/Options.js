099b5ae4bcefcdec427186309314d7f696c61877
alex <am3081@gmail.com>
2019-10-02 16:39:45 -0400

    new things

--- src/components/censusCharts/Options.js
+++ src/components/censusCharts/Options.js
@@ -23,7 +23,7 @@ export default class Options extends React.Component {
       <div className="onboarding-modal modal fade animated show" id="onboardingSlideModal" role="dialog" tabIndex={-1} style={{display: 'block', paddingRight: '15px'}}>
         <div className="modal-dialog modal-centered" role="document">
           <div className="modal-content text-center">
-            <button aria-label="Close" className="close" data-dismiss="modal" type="button"><span className="close-label">Close</span><span className="os-icon os-icon-close" /></button>
+            <button aria-label="Close" className="close" data-dismiss="modal" type="button"><span className="close-label">Skip Intro</span><span className="os-icon os-icon-close" /></button>
             <div className="onboarding-slider-w slick-initialized slick-slider slick-dotted">
               <button className="slick-prev slick-arrow slick-disabled" aria-label="Previous" type="button" aria-disabled="true" style={{}}>Previous</button>
               <div className="slick-list draggable" style={{height: '599.594px'}}>
@@ -32,8 +32,57 @@ export default class Options extends React.Component {
                     <div className="onboarding-media"><img alt="" src="img/bigicon5.png" width="200px" /></div>
                     <div className="onboarding-content with-gradient">
                       <h4 className="onboarding-title">Example Request Information</h4>
-                      <div className="onboarding-text">Test 123</div>
-                      
+                      <div className="onboarding-text">In this example you can see a form where you can request some additional information from the customer when they land on the app page.</div>
+                      <form>
+                        <div className="row">
+                          <div className="col-sm-6">
+                            <div className="form-group">
+                              <label htmlFor>Your Full Name</label>
+                              <input className="form-control" placeholder="Enter your full name..." type="text" defaultValue tabIndex={0} />
+                            </div>
+                          </div>
+                          <div className="col-sm-6">
+                            <div className="form-group">
+                              <label htmlFor>Your Role</label>
+                              <select className="form-control" tabIndex={0}>
+                                <option>Web Developer</option>
+                                <option>Business Owner</option>
+                                <option>Other</option>
+                              </select>
+                            </div>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                  <div className="onboarding-slide slick-slide" data-slick-index={1} aria-hidden="true" style={{width: '550px'}} tabIndex={-1} role="tabpanel" id="slick-slide01" aria-describedby="slick-slide-control01">
+                    <div className="onboarding-media"><img alt="" src="img/bigicon6.png" width="200px" /></div>
+                    <div className="onboarding-content with-gradient">
+                      <h4 className="onboarding-title">Showcase App Features</h4>
+                      <div className="onboarding-text">In this example you can showcase some of the features of your application, it is very handy to make new users aware of your hidden features. You can use boostrap columns to split them up.</div>
+                      <div className="row">
+                        <div className="col-sm-6">
+                          <ul className="features-list">
+                            <li>Fully Responsive design</li>
+                            <li>Pre-built app layouts</li>
+                            <li>Incredible Flexibility</li>
+                          </ul>
+                        </div>
+                        <div className="col-sm-6">
+                          <ul className="features-list">
+                            <li>Boxed &amp; Full Layouts</li>
+                            <li>Based on Bootstrap 4</li>
+                            <li>Developer Friendly </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="onboarding-slide slick-slide" data-slick-index={2} aria-hidden="true" style={{width: '550px'}} tabIndex={-1} role="tabpanel" id="slick-slide02" aria-describedby="slick-slide-control02">
+                    <div className="onboarding-media"><img alt="" src="img/bigicon2.png" width="200px" /></div>
+                    <div className="onboarding-content with-gradient">
+                      <h4 className="onboarding-title">Example of onboarding screen!</h4>
+                      <div className="onboarding-text">This is an example of a multistep onboarding screen, you can use it to introduce your customers to your app, or collect additional information from them before they start using your app.</div>
                     </div>
                   </div>
                 </div>
@@ -58,6 +107,7 @@ export default class Options extends React.Component {
   }
   
   toggleModal() {
+    console.log('toggle')
     this.setState({showModal: !this.state.showModal})
   }
 
@@ -72,7 +122,7 @@ export default class Options extends React.Component {
       } }>
        <ul className="nav nav-tabs smaller">
           <li className="nav-item" style={ { margin: 0 } }>
-            <A className="nav-link" data-toggle="tab" onClick={this.toggleModal}>View Data</A>
+            <A className="nav-link"  onClick={this.toggleModal}>View Data</A>
           </li>
           <li className="nav-item" style={ { margin: 0 } }>
             <A className="nav-link" data-toggle="tab" href="#">Save Image</A>
@@ -81,7 +131,7 @@ export default class Options extends React.Component {
             <A className="nav-link" data-toggle="tab" href="#">Share Embed</A>
           </li>
        </ul>
-       
+       {this.state.showModal ? this.renderModal() : ''}
     </div>
     );
   }
